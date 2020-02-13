2746f47c26aef67991d366a992ce081be5af402b
El'dar Nasyrov <buugaaga@bk.ru>
2020-02-13 14:07:25 +0500

    Updates

---
+++ precache-manifest.0167ce722ac92e74fc2e0b94ba3362d0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49408d1d6b3ce99f6c1f706332393a53",
+    "url": "/game-cover/index.html"
+  },
+  {
+    "revision": "4511e6543f093e4f2c31",
+    "url": "/game-cover/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0edd6602f8c1901ebce",
+    "url": "/game-cover/static/js/2.5b024e9d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/game-cover/static/js/2.5b024e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4511e6543f093e4f2c31",
+    "url": "/game-cover/static/js/main.b5a3babb.chunk.js"
+  },
+  {
+    "revision": "7f7a8220d68f27c7c360",
+    "url": "/game-cover/static/js/runtime-main.62c12bfe.js"
+  },
+  {
+    "revision": "972e8bca00507601c1aba3b50f668eb7",
+    "url": "/game-cover/static/media/background-screen.972e8bca.png"
+  },
+  {
+    "revision": "3a11c8fa2be538fba390e3a894c952ba",
+    "url": "/game-cover/static/media/background.3a11c8fa.png"
+  },
+  {
+    "revision": "7bcf559d961276cc05aef857f9856ca6",
+    "url": "/game-cover/static/media/header.7bcf559d.png"
+  },
+  {
+    "revision": "324c7fbc8517cacee7f1f4bcbdfae262",
+    "url": "/game-cover/static/media/side-bg.324c7fbc.jpg"
+  },
+  {
+    "revision": "159c96d77084cc6ce6a62929b3596c79",
+    "url": "/game-cover/static/media/task-kings.159c96d7.png"
+  },
+  {
+    "revision": "94ef6c8d31b03b1ad7e56f54cb9dc246",
+    "url": "/game-cover/static/media/task-time.94ef6c8d.png"
+  },
+  {
+    "revision": "935c7e99ed4f2d613c6fef68efcf0fc0",
+    "url": "/game-cover/static/media/task-tournaments.935c7e99.png"
+  }
+]);
